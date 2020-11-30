5c105ab89f37a7559422435170ea14d2470fc030
zacky77-git <miyazaki.ytmym@me.com>
2020-11-30 19:21:24 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,9 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all  # すべてのレコードを@postsに代入
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  
 
   def create
   Post.create(content: params[:content])
